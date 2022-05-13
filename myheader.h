710eaa4bcd00f9652733252069fd6c323306951e
Konovalov Andrey Sergeevich <andrrrey.konovalov@gmail.com>
2022-05-13 19:00:32 +0300

    my_queue; merge;

--- myheader.h
+++ myheader.h
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <functional>
+
+#include <algorithm>
 #include <random>
 #include <time.h>
+
 #include <thread>
 #include <mutex>
+
 #include <string>
-#include <algorithm>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -14,18 +20,45 @@ class Solution{
 public:
     Solution();
     Solution(string problem_fname, string solution_fname);
-    void Multithread_calculation();
+    void Multithread_calculation( int req_num_treads );
 
     void demo();
 
+    void thread_run( FILE * problem );
+
+    // temporary public:
+    void merge_files(string fn1, string fn2);
+
 private:
 
-    mutex common_mutex;
+    mutex reading_mutex,
+          queue_mutex;
     string prob,
            sol;
     int split_counter;
+    int threads_working;
 
     void split_to_sorted(FILE * f_ptr, int n, string &common_name);
-    void merge_files(string fn1, string fn2);
 
+};
+
+struct fpair{
+    string fn1, fn2;
+};
+
+class my_queue{
+public:
+    my_queue();
+
+    void push( string& s );
+    fpair extract_front();
+    bool not_empty;
+
+    string get_remains();
+    bool expecting;
+
+private:
+
+    string tmp_hold;
+    queue<fpair> Q;
 };
--- source.cpp
+++ source.cpp
@@ -4,33 +4,39 @@ Solution::Solution(){
     prob = "data";
     sol = "result";
     split_counter = 0;
+    threads_working = 0;
 }
 
 Solution::Solution( string problem_fname, string solution_fname ){
     prob = problem_fname;
     sol = solution_fname;
     split_counter = 0;
+    threads_working = 0;
 }
 
 void Solution::split_to_sorted(FILE * f_ptr, int n, string &common_name){
 
     while(true){
 
-        common_mutex.lock();
+        reading_mutex.lock();
 
             // reading another portion of numbers into vector
             vector<int> read(n); // underlying storage of std::vector is also an array
             size_t realsize = fread(read.data(), sizeof(read[0]), read.size(), f_ptr);
 
-            if(realsize==0)
+            if(realsize==0){
+                // unlocking other threads before leaving the cycle-body:
+                reading_mutex.unlock();
                 break;
+            }
             else if(realsize<n)
                 read.resize(realsize);
 
             split_counter++;
             int current_index = split_counter;
 
-        common_mutex.unlock();
+        // if the cycle is not left, unlocking here:
+        reading_mutex.unlock();
 
         sort(read.begin(),read.end());
 
@@ -44,21 +50,105 @@ void Solution::split_to_sorted(FILE * f_ptr, int n, string &common_name){
 
 }
 
-void Solution::Multithread_calculation(){
+void Solution::thread_run( FILE * problem ){
 
-
-    FILE * problem = fopen(prob.c_str(),"rb");
     string common_parts_name_prefix = "part_";
     int n_in_splitted = 10;
     split_to_sorted( problem, n_in_splitted, common_parts_name_prefix );
-    fclose(problem);
 
+}
+
+void Solution::Multithread_calculation( int req_num_treads ){
+
+	int max_threads = thread::hardware_concurrency();
+	int num_threads = min(max_threads,req_num_treads);
+
+	// cout << num_threads << "\n";
+
+    FILE * problem = fopen(prob.c_str(),"rb");
+
+    // threads begin their work
+	vector<thread> threads(num_threads-1);
+    for(int i=0; i<num_threads-1; i++)
+          threads[i] = thread(&Solution::thread_run, this, ref(problem));
+    for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
+    // and they are done by now
+
+    fclose(problem);
 
 }
 
 void Solution::merge_files(string fn1, string fn2){
+    FILE * f1 = fopen(fn1.c_str(),"rb");
+    FILE * f2 = fopen(fn2.c_str(),"rb");
 
+    string outfname = ( *(fn1.end()-1)=='_' ) ?
+                        string(fn1.begin(),fn1.end()-1) : (fn1 + "_");
+    FILE * outf = fopen(outfname.c_str(),"wb");
+
+    int i1, i2;
+    int s_i1 = fread(&i1,sizeof(int),1,f1),
+        s_i2 = fread(&i2,sizeof(int),1,f2);
+
+    while( s_i1*s_i2>0 ){
+        if(i2<i1){
+            fwrite(&i2,sizeof(int),1,outf);
+            s_i2 = fread(&i2,sizeof(int),1,f2);
+        }
+        else{
+            fwrite(&i1,sizeof(int),1,outf);
+            s_i1 = fread(&i1,sizeof(int),1,f1);
+        }
+    }
+    if(s_i1==0)
+        while(s_i2>0){
+            fwrite(&i2,sizeof(int),1,outf);
+            s_i2 = fread(&i2,sizeof(int),1,f2);
+        }
+    else
+        while(s_i1>0){
+            fwrite(&i1,sizeof(int),1,outf);
+            s_i1 = fread(&i1,sizeof(int),1,f1);
+        }
+
+    fclose(outf);
+    fclose(f1);
+    fclose(f2);
+
+    remove(fn1.c_str());
+    remove(fn2.c_str());
 }
+
+
+
+my_queue::my_queue(){
+    not_empty = false;
+    expecting = false;
+}
+
+void my_queue::push( string& s ){
+    if(expecting){
+        Q.push( {tmp_hold, s} );
+        expecting = false;
+        not_empty = true;
+    }
+    else{
+        tmp_hold = s;
+        expecting = true;
+    }
+}
+
+fpair my_queue::extract_front(){
+    fpair tmp = Q.front();
+    Q.pop();
+    not_empty = !Q.empty();
+    return tmp;
+}
+
+string my_queue::get_remains(){
+    return tmp_hold;
+}
+//===
 
 void Solution::demo(){
     // checking the contents of new files
